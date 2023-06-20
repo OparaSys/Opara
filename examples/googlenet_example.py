797562a8961566cbdac3cdfcd428da986320597d
OparaSys <134792133+OparaSys@users.noreply.github.com>
2023-06-20 11:22:31 +0800

    Add files via upload

--- examples/googlenet_example.py
+++ examples/googlenet_example.py
@@ -1,21 +1,21 @@
 import torch
 import torchvision
-from Opara import GraphCapturer
 import numpy as np
+from Opara import GraphCapturer
 
-def run_torch_model(model, x_torch, iterations, warm_ups):
+def run_torch_model(model, inputs, iterations, warm_ups):
     start_events = [torch.cuda.Event(enable_timing=True) for _ in range(iterations)]
     end_events = [torch.cuda.Event(enable_timing=True) for _ in range(iterations)]
     # model = torch.compile(model)
     # Warmup steps
     for _ in range(warm_ups):
-        model(x_torch) # don't record time
+        model(*inputs) # don't record time
     for i in range(iterations):
         # x_torch = torch.randint(low=0, high=256, size=(1, 3, 224, 224), dtype=torch.float32).to(device="cuda:0")
         flush_cache()
         torch.cuda._sleep(1_000_000)
         start_events[i].record()
-        y = model(x_torch)
+        y = model(*inputs)
         end_events[i].record()
     torch.cuda.synchronize()
     times = [s.elapsed_time(e) for s, e in zip(start_events, end_events)]
@@ -24,17 +24,17 @@ def run_torch_model(model, x_torch, iterations, warm_ups):
     print('{:<30} {:<20} {:<20}'.format(*output_str))
     return y
 
-def run_sequence_graph(symbolic_traced, x1, iterations, warm_ups, start_index, end_index):
+def run_sequence_graph(symbolic_traced, inputs, iterations, warm_ups, start_index, end_index):
     with torch.no_grad():
         for i in range(warm_ups):
-            symbolic_traced(x1)
-            # my_interpreter.run(x1)
+            symbolic_traced(*inputs)
+            # my_interpreter.run(inputs)
 
     with torch.no_grad():
         g1 = torch.cuda.CUDAGraph()
 
         with torch.cuda.graph(g1):
-            out_torch_graph_without_stream = symbolic_traced(x1)
+            out_torch_graph_without_stream = symbolic_traced(*inputs)
 
         time_list = []
         torch.cuda.synchronize()
@@ -84,14 +84,17 @@ if __name__ == '__main__':
     iterations = 30
     x = torch.randint(low=0, high=256, size=(1, 3, 224, 224), dtype=torch.float32).to(device="cuda:0")
     model = torchvision.models.googlenet().eval()
+
+    inputs = (x,)
     model = model.to(device="cuda:0").eval()
-    y = run_torch_model(model, x, iterations, warm_ups)
+    y = run_torch_model(model, inputs, iterations, warm_ups)
 
-    run_sequence_graph(model, x, iterations, warm_ups, 0, 300)
+    run_sequence_graph(model, inputs, iterations, warm_ups, 0, 300)
 
-
-    Opara = GraphCapturer.capturer((x,), model)
-    output = run_parallel_graph(Opara, (x,), iterations, warm_ups, 0, 300)
-
-    print("output of PyTorch == output of Opara:", torch.allclose(y, output[0],rtol=1e-07,atol=1e-07,equal_nan =False), end='     ')
-    print('Absolute difference:', torch.max(torch.abs(y.detach() - output[0].detach())))
+    Opara = GraphCapturer.capturer(inputs, model)
+    output = run_parallel_graph(Opara, inputs, iterations, warm_ups, 0, 300)
+    res = output[0]
+    if res.dtype == torch.float16:
+        res = res.float()
+    print("output of PyTorch == output of Opara:", torch.allclose(y, res,rtol=1e-05,atol=1e-05,equal_nan =False), end='     ')
+    print('Absolute difference:', torch.max(torch.abs(y.detach() - res.detach())))
